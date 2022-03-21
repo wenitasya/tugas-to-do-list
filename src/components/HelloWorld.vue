d95b71406e33b6b9c7139f85dc94276cb46ad745
wenitasya <wenitasya2000@gmail.com>
2022-03-21 11:32:56 +0700

    nyoba

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,57 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <h1 class="text-center"> Welcome to Todolist App</h1>
+    <div class="box"> 
+      <div class="row mt-5" style="center;">
+      <div class="col-xs-12 col-md-4">
+        <form>
+          <div class="form">
+            <label for="newtodolist"></label>
+            <input type="text" class="form-control" size="10" ref="textbox" v-model="newtodolist" placeholder="Input to do list here...">
+          </div>
+            <button type="button" class="btn btn-warning" v-on:click="addTodo">Save</button>
+        </form>
+      </div>
+         <div class="col-xs-12 col-md-8 pt-4">
+          <ul class="list">
+            <li class="list-group-item disabled" v-for="(todolist,index) in todolists" :key="index">
+            {{todolist.text}}
+            <button type="button" class="btn btn-danger float-right" v-on:click="removeTodo(index)">Remove</button>
+            </li>
+          </ul>
+         </div>
+    </div></div>
+   
   </div>
+
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  data() {
+        return {
+        todolists :[
+        {text : "Task 1"},
+        {text : "Task 2"},
+        ],
+        newtodolist : ""
+        }
+  },
+  methods: {
+      addTodo:function(){
+        this.todolists.push({text:this.newtodolist});
+        this.$refs.textbox.focus()
+        this.newtodolist = "" 
+        },
+      removeTodo:function(index){
+        this.todolists.splice(index,1);
+        }
+  },
 }
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+ </script>
+ <style>
+ .box {
+   text-align: center;
+ }
+ </style>
